2e379d36c378c3e63e88cecd503a9967dbded882
s3633197 <s3633197@student.rmit.edu.au>
2021-05-31 20:26:10 +0800

    first commit

---
+++ infra/terraform.tfvars
@@ -0,0 +1,8 @@
+subnet_ids = [
+	"subnet-01bd16cc8e9bb42b2",
+	"subnet-0c17268576eb331d0"
+]
+username = "AWS"
+password = "12345678"
+vpc_id = "vpc-00b7d45dd508d4397"
+name = "todo-db"
---
+++ src/.qawolf/tests/login.test.js
@@ -0,0 +1,36 @@
+const { launch } = require("qawolf");
+const selectors = require("../selectors/login");
+
+describe('login', () => {
+  let browser;
+
+  beforeAll(async () => {
+    browser = await launch({ url: "http://aa92c5deccd254d9b9f0bda88897dda8-1982141712.us-east-1.elb.amazonaws.com:443" });
+  });
+
+  afterAll(() => browser.close());
+  
+  it('can click "Login" link', async () => {
+    await browser.click(selectors[0]);
+  });
+  
+  it('can click "email" input', async () => {
+    await browser.click(selectors[1]);
+  });
+  
+  it('can type into "email" input', async () => {
+    await browser.type(selectors[2], "ben@rmit.edu.au");
+  });
+  
+  it("can Tab", async () => {
+    await browser.type(selectors[3], "↓Tab↑Tab");
+  });
+  
+  it('can type into "password" input', async () => {
+    await browser.type(selectors[4], "1234");
+  });
+  
+  it('can click "Submit" button', async () => {
+    await browser.click(selectors[5]);
+  });
+});
