d1f7749a63e1eea3d64bedaf4cc5735936aa0aa6
s3633197 <s3633197@student.rmit.edu.au>
2021-06-02 20:04:09 +0800

    a

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: process.env.DB_URL || "mongodb://localhost:27017/tododb"
+  mongoURI: process.env.DB_URL || "mongodb://AWS:password@todo-db-test-docdb-cluster.cluster-ctioohynuogq.us-east-1.docdb.amazonaws.com:27017"
 }
